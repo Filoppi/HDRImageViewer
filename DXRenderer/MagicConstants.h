0b54c91a97e0567a848b9e411b0c1b2290cd684f
Filoppi <filippotarpini@hotmail.it>
2024-01-14 15:24:33 +0200

    More experimental stuff

--- DXRenderer/MagicConstants.h
+++ DXRenderer/MagicConstants.h
@@ -1,12 +1,12 @@
 #pragma once
 
-static const float sc_DefaultHdrDispMaxNits = 600.0f; // Conservative default, prevents egregious behavior on low spec displays.
+static const float sc_DefaultHdrDispMaxNits = 750.0f; // Conservative default, prevents egregious behavior on low spec displays.
 static const float sc_DefaultSdrDispMaxNits = 300.0f; // Empirically chosen to produce reasonable results on typical SDR displays.
 // Note that going below this target nits value can produce artifacts in the OS tone mapper.
 static const float sc_DefaultPaperWhiteNits = 203.0f; // Based on BT.2100 recommended SDR viewing conditions.
-static const float sc_DefaultImageMaxCLL = 600.0f; // Needs more tuning based on real world content.
-static const float sc_DefaultImageMedCLL = 80.0f; // Needs more tuning based on real world content.
-static const float sc_MaxZoom = 1.0f; // Restrict max zoom to 1:1 scale.
+static const float sc_DefaultImageMaxCLL = 750.0f; // Needs more tuning based on real world content.
+static const float sc_DefaultImageMedCLL = 80.0f; // Needs more tuning based on real world content (this is way too high).
+static const float sc_MaxZoom = 2.5f; // Restrict max zoom to 1:1 scale.
 static const float sc_MinZoomSphereMap = 0.25f;
 
 // n bins with gamma of 10 lets us measure luminance to within 10% error for any
